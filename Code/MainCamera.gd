86c08b4e866143b7eb4bf018ea19290b8210a4f8
Raul Huertas <raul.huertas@amplia.es>
2019-08-31 23:04:43 +0200

    Trying to make camera drag withou glitches

--- Code/MainCamera.gd
+++ Code/MainCamera.gd
@@ -8,31 +8,32 @@ var _current_zoom_level: float = 4.18
 var _drag: bool = false
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("cam_drag"):
-        _drag = true
-    elif event.is_action_released("cam_drag"):
-        _drag = false
-    elif event.is_action("cam_zoom_in"):
-        _update_zoom(-ZOOM_INCREMENT, get_local_mouse_position())
-    elif event.is_action("cam_zoom_out"):
-        _update_zoom(ZOOM_INCREMENT, get_local_mouse_position())
-    elif event is InputEventMouseMotion && _drag:
-        set_offset(get_offset() - event.relative*_current_zoom_level)
+	if event.is_action_pressed("cam_drag"):
+		_drag = true
+	elif event.is_action_released("cam_drag"):
+		_drag = false
+	elif event.is_action("cam_zoom_in"):
+		_update_zoom(-ZOOM_INCREMENT, get_local_mouse_position())
+	elif event.is_action("cam_zoom_out"):
+		_update_zoom(ZOOM_INCREMENT, get_local_mouse_position())
+	elif event is InputEventMouseMotion && _drag:
+		var new_offset = get_offset() - event.relative*_current_zoom_level
+		set_offset(new_offset)
+		print(new_offset)
 
 
 func _update_zoom(incr: float, zoom_anchor: Vector2) -> void:
-    var old_zoom = _current_zoom_level
-    _current_zoom_level += incr
-    if _current_zoom_level < MAX_ZOOM_LEVEL:
-        _current_zoom_level = MAX_ZOOM_LEVEL
-    elif _current_zoom_level > MIN_ZOOM_LEVEL:
-        _current_zoom_level = MIN_ZOOM_LEVEL
-    if old_zoom == _current_zoom_level:
-        return
-    
-    var zoom_center = zoom_anchor - get_offset()
-    var ratio = 1-_current_zoom_level/old_zoom
-    set_offset(get_offset() + zoom_center*ratio)
-    
-    set_zoom(Vector2(_current_zoom_level, _current_zoom_level))
-
+	var old_zoom = _current_zoom_level
+	_current_zoom_level += incr
+	if _current_zoom_level < MAX_ZOOM_LEVEL:
+		_current_zoom_level = MAX_ZOOM_LEVEL
+	elif _current_zoom_level > MIN_ZOOM_LEVEL:
+		_current_zoom_level = MIN_ZOOM_LEVEL
+	if old_zoom == _current_zoom_level:
+		return
+		
+	var zoom_center = zoom_anchor - get_offset()
+	var ratio = 1-_current_zoom_level/old_zoom
+	set_offset(get_offset() + zoom_center*ratio)
+		
+	set_zoom(Vector2(_current_zoom_level, _current_zoom_level))
