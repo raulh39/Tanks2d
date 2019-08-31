e3fb8c6f1ee819cb61fafed1f527fcc1cd0a1ea9
Raul Huertas <raul.huertas@amplia.es>
2019-08-31 22:33:08 +0200

    Game begins now to max zoom level

--- Code/MainCamera.gd
+++ Code/MainCamera.gd
@@ -1,10 +1,10 @@
 extends Camera2D
 
 const MAX_ZOOM_LEVEL = 0.5
-const MIN_ZOOM_LEVEL = 4.0
+const MIN_ZOOM_LEVEL = 4.18
 const ZOOM_INCREMENT = 0.05
 
-var _current_zoom_level = 1
+var _current_zoom_level = 4.18
 var _drag = false
 
 func _input(event):
