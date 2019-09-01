6b8a0998b6b5221b56ee715f02b5aac7361e0cbd
Raul Huertas <raul.huertas@amplia.es>
2019-09-01 14:58:05 +0200

    Fix mouse dragging out off limits

--- Code/MainCamera.gd
+++ Code/MainCamera.gd
@@ -1,10 +1,12 @@
 extends Camera2D
 
-const MAX_ZOOM_LEVEL: float = 0.5
-const MIN_ZOOM_LEVEL: float = 4.18
+const MIN_ZOOM_LEVEL: float = 0.5
+const MAX_ZOOM_LEVEL: float = 4.17
 const ZOOM_INCREMENT: float = 0.05
 
-onready var _current_zoom_level: float = self.zoom.x
+#Errors in this code:
+#   - doesn't zoom centering at mouse position
+
 var _drag: bool = false
 
 func _input(event: InputEvent) -> void:
@@ -13,27 +15,27 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_released("cam_drag"):
 		_drag = false
 	elif event.is_action("cam_zoom_in"):
-		_update_zoom(-ZOOM_INCREMENT, get_local_mouse_position())
+		_update_zoom(-ZOOM_INCREMENT)
 	elif event.is_action("cam_zoom_out"):
-		_update_zoom(ZOOM_INCREMENT, get_local_mouse_position())
+		_update_zoom(ZOOM_INCREMENT)
 	elif event is InputEventMouseMotion && _drag:
-		var new_offset = get_offset() - event.relative*_current_zoom_level
-		set_offset(new_offset)
-		print(new_offset)
+		var new_offset := offset - Vector2(event.relative.x*zoom.x, event.relative.y*zoom.y)
+		new_offset.x = clamp(new_offset.x, limit_left, limit_right-OS.window_size.x*zoom.x)
+		new_offset.y = clamp(new_offset.y, limit_top, limit_bottom-OS.window_size.y*zoom.y)
+		offset = new_offset
 
 
-func _update_zoom(incr: float, zoom_anchor: Vector2) -> void:
-	var old_zoom = _current_zoom_level
-	_current_zoom_level += incr
-	if _current_zoom_level < MAX_ZOOM_LEVEL:
-		_current_zoom_level = MAX_ZOOM_LEVEL
-	elif _current_zoom_level > MIN_ZOOM_LEVEL:
-		_current_zoom_level = MIN_ZOOM_LEVEL
-	if old_zoom == _current_zoom_level:
+func _update_zoom(incr: float) -> void:
+	var new_zoom := Vector2(
+		clamp(zoom.x+incr, MIN_ZOOM_LEVEL, MAX_ZOOM_LEVEL),
+		clamp(zoom.y+incr, MIN_ZOOM_LEVEL, MAX_ZOOM_LEVEL)
+	)
+	if new_zoom == zoom:
 		return
-		
-	var zoom_center = zoom_anchor - get_offset()
-	var ratio = 1-_current_zoom_level/old_zoom
-	set_offset(get_offset() + zoom_center*ratio)
-		
-	set_zoom(Vector2(_current_zoom_level, _current_zoom_level))
+	
+	offset = get_local_mouse_position()
+	zoom = new_zoom
+	print("New zoom: " + str(zoom))
+	# var zoom_center = mouse_position - offset
+	# var ratio = 1-_current_zoom_level____________/old_zoom
+	# offset = offset + zoom_center*ratio
