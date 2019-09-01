0be5fd89cde51c6e5fa4d10237e8e348ba6045fd
Raul Huertas <raul.huertas@amplia.es>
2019-09-01 20:42:12 +0200

    Fix mouse drag and zoom

--- Code/MainCamera.gd
+++ Code/MainCamera.gd
@@ -1,11 +1,12 @@
 extends Camera2D
 
-const MIN_ZOOM_LEVEL: float = 0.5
-const MAX_ZOOM_LEVEL: float = 4.17
-const ZOOM_INCREMENT: float = 0.05
+func clamp_offset() -> void:
+		offset.x = clamp(offset.x, limit_left, limit_right  - OS.window_size.x * zoom.x)
+		offset.y = clamp(offset.y, limit_top,  limit_bottom - OS.window_size.y * zoom.y)
 
-#Errors in this code:
-#   - doesn't zoom centering at mouse position
+const MIN_ZOOM_LEVEL: float = 0.5
+const MAX_ZOOM_LEVEL: float = 4500.0/1080.0 #(limit_right-limit_left)/OS.window_size.x
+const ZOOM_INCREMENT: float = 0.05
 
 var _drag: bool = false
 
@@ -19,10 +20,8 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action("cam_zoom_out"):
 		_update_zoom(ZOOM_INCREMENT)
 	elif event is InputEventMouseMotion && _drag:
-		var new_offset := offset - Vector2(event.relative.x*zoom.x, event.relative.y*zoom.y)
-		new_offset.x = clamp(new_offset.x, limit_left, limit_right-OS.window_size.x*zoom.x)
-		new_offset.y = clamp(new_offset.y, limit_top, limit_bottom-OS.window_size.y*zoom.y)
-		offset = new_offset
+		offset -= Vector2(event.relative.x*zoom.x, event.relative.y*zoom.y)
+		clamp_offset()
 
 
 func _update_zoom(incr: float) -> void:
@@ -33,9 +32,9 @@ func _update_zoom(incr: float) -> void:
 	if new_zoom == zoom:
 		return
 	
-	offset = get_local_mouse_position()
+	var old_mp := get_local_mouse_position()
 	zoom = new_zoom
-	print("New zoom: " + str(zoom))
-	# var zoom_center = mouse_position - offset
-	# var ratio = 1-_current_zoom_level____________/old_zoom
-	# offset = offset + zoom_center*ratio
+	var new_mp := get_local_mouse_position()
+	var diff := new_mp - old_mp
+	offset -= diff
+	clamp_offset()
