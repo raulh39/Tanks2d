09eecd5aaf67f45d8c2e2577684f404fb79289ed
Raul Huertas <raulh39@gmail.com>
2019-09-07 22:24:37 +0200

    Add vehicle variables

--- Scenes/Tanks/Vehicle.gd
+++ Scenes/Tanks/Vehicle.gd
@@ -1,5 +1,25 @@
 extends Area2D
 
+enum Countries {
+	German,
+	British,
+	American,
+	Soviet
+}
+
+enum Abilities {
+	gung_ho,
+	blitzkrieg
+}
+
+export (String) var tank_name
+export (Countries) var country
+export (int, 0, 200) var point_value
+export (int, 0, 6) var initiative
+export (int, 0, 6) var attack
+export (int, 0, 6) var defense
+export (int, 0, 6) var damage_capacity
+export (Array, Abilities) var abilities = []
 
 func _on_Vehicle_mouse_entered():
 	$HullGlow.visible = true
