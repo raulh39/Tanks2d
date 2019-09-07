7e4ec86f26b889b902257c87ce65e727ac7e9e05
Raul Huertas <raulh39@gmail.com>
2019-09-08 00:30:33 +0200

    Adjust initiative valid values

--- Scenes/Tanks/Vehicle.gd
+++ Scenes/Tanks/Vehicle.gd
@@ -15,7 +15,7 @@ enum Abilities {
 export (String) var tank_name
 export (Countries) var country
 export (int, 0, 200) var point_value
-export (int, 0, 6) var initiative
+export (int, 0, 10) var initiative
 export (int, 0, 6) var attack
 export (int, 0, 6) var defense
 export (int, 0, 6) var damage_capacity
