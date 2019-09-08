70b08b1522d5ef227a6140ceaab4757c392c707c
Raul Huertas <raulh39@gmail.com>
2019-09-08 19:03:05 +0200

    Add GUI

---
+++ Scenes/GUI.gd
@@ -0,0 +1,16 @@
+extends MarginContainer
+
+class_name Gui
+
+enum Phases { MOVE, SHOOT, COMMAND }
+
+onready var _phase_label: Label = ($HBoxContainer/PhaseLabel as Label)
+
+func setPhase(new_phase: int) -> void:
+	match new_phase:
+		Phases.MOVE:
+			_phase_label.text = "Movement Phase"
+		Phases.SHOOT:
+			_phase_label.text = "Shooting Phase"
+		Phases.COMMAND:
+			_phase_label.text = "Command Phase"
