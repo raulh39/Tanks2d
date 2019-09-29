725c840c806c9a3c54993f51921d0c2edc9e2da2
Raul Huertas <raulh39@gmail.com>
2019-09-29 19:42:09 +0200

    Add OnExit confirmation dialog

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -4,4 +4,7 @@ func _on_New_Game_pressed():
 	get_tree().change_scene("res://Scenes/Main.tscn")
 
 func _on_Quit_Game_pressed():
+	$ConfirmationDialog.popup()
+
+func _on_ConfirmationDialog_confirmed():
 	get_tree().quit()
