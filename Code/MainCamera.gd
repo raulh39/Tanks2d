9ebe8a4e5f3d5dd3251bf7e178b2090dee04fe1f
Raul Huertas <raulh39@gmail.com>
2019-10-19 18:40:40 +0200

    Center board game on screen

--- Code/MainCamera.gd
+++ Code/MainCamera.gd
@@ -1,12 +1,18 @@
 extends Camera2D
 
-func clamp_offset() -> void:
-		offset.x = clamp(offset.x, -500, limit_right  - OS.window_size.x * zoom.x+500)
-		offset.y = clamp(offset.y, -500, limit_bottom - OS.window_size.y * zoom.y+500)
-#	if zoom.x < MAX_ZOOM_LEVEL:
-#	else:
-#		offset.x = 0
-#		offset.y = 0
+func clamp_position() -> void:
+	var pixels_shown_by_camera := OS.window_size * zoom
+	print("pixels_shown_by_camera: ", pixels_shown_by_camera)
+	print("prev position: ", position)
+	if pixels_shown_by_camera.x < 5500:
+		position.x = clamp(position.x, -500, 5000 - pixels_shown_by_camera.x)
+	else:
+		position.x = (4500 - pixels_shown_by_camera.x)/2
+	if pixels_shown_by_camera.y < 5500:
+		position.y = clamp(position.y, -500, 5000 - pixels_shown_by_camera.y)
+	else:
+		position.y = (4500 - pixels_shown_by_camera.y)/2
+	print("post position: ", position)
 
 const MIN_ZOOM_LEVEL: float = 0.5
 var MAX_ZOOM_LEVEL: float
@@ -15,6 +21,7 @@ const ZOOM_INCREMENT: float = 0.05
 func _ready():
 	MAX_ZOOM_LEVEL = (4500+500+500) / OS.window_size.y
 	zoom = Vector2(MAX_ZOOM_LEVEL, MAX_ZOOM_LEVEL)
+	clamp_position()
 
 var _drag: bool = false
 
@@ -28,8 +35,8 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action("cam_zoom_out"):
 		_update_zoom(ZOOM_INCREMENT)
 	elif event is InputEventMouseMotion && _drag:
-		offset -= Vector2(event.relative.x*zoom.x, event.relative.y*zoom.y)
-		clamp_offset()
+		position -= Vector2(event.relative.x*zoom.x, event.relative.y*zoom.y)
+		clamp_position()
 
 
 func _update_zoom(incr: float) -> void:
@@ -44,5 +51,5 @@ func _update_zoom(incr: float) -> void:
 	zoom = new_zoom
 	var new_mp := get_local_mouse_position()
 	var diff := new_mp - old_mp
-	offset -= diff
-	clamp_offset()
+	position -= diff
+	clamp_position()
