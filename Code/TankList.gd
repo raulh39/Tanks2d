f790769910ffb97cbef8be76ab3020f9884af3a3
Raul Huertas <raulh39@gmail.com>
2019-10-27 12:15:04 +0100

    Refactor TankList.gd

--- Code/TankList.gd
+++ Code/TankList.gd
@@ -2,23 +2,61 @@ extends Node2D
 
 signal vehicle_selected(tank)
 
-func _connect():
-	for t in get_children():
-		(t as Vehicle).connect("vehicle_selected", self, "_vehicle_selected")
-func _disconnect():
-	for t in get_children():
-		(t as Vehicle).disconnect("vehicle_selected", self, "_vehicle_selected")
-
-func _vehicle_selected(vehicle):
-	emit_signal("vehicle_selected", vehicle)
-
+#------------------
+# Tanks come into play in initiative order. In the movement phase they go from 
+# lower initiative to higher but in the shooting phase they go from higher to 
+# lower.
+# As Godot still hasn't lambdas, we must make two static functions, "gt" and 
+# "lt" just to order the tanks in one way or the other.
+#------------------
 static func lt(x:int , y: int) -> bool:
     return x < y
 
 static func gt(x:int , y: int) -> bool:
     return x > y
 
-func _get_tanks_by_initiative(comparator: FuncRef) -> Array:
+#-----------------------------------------
+# Tanks movement
+#-----------------------------------------
+func move_tanks() -> void:
+	_reset_acted()
+	while _more_to_act():
+		var tank:Vehicle = yield(_select_tank_by_initiative(funcref(self, "lt")), "completed")
+		yield(tank.move_tank(), "completed")
+		tank.has_acted = true
+
+func _reset_acted()->void:
+	for i in get_children():
+		(i as Vehicle).has_acted = false
+
+func _more_to_act() -> bool:
+	for i in get_children():
+		if not (i as Vehicle).has_acted:
+			return true
+	return false
+
+#----------------
+# Tanks are selected in initiative order. If only one has the greatest (or 
+# lowest) initiative, that is selected. But if more than one has the same 
+# greatest (or lowest) initiative the user should select one of them.
+# We must, in that case, wait until the user clicks in one of the selectable
+# tanks, that is, we must wait for the signal "vehicle_selected". So we
+# must use "yield" to wait until that signal is sent.
+#----------------
+func _select_tank_by_initiative(comparator: FuncRef) -> Vehicle:
+	var tanks_allowed_to_act := _get_next_group_to_act(comparator)
+	var tank: Vehicle
+	if tanks_allowed_to_act.size() > 1:
+		tank = yield(_select(tanks_allowed_to_act), "completed") as Vehicle
+	else:
+		#If this branch doesn't yield, the returned object is not a coroutine and the code doesn't works
+		#so we have to fake a yield.
+		yield(get_tree().create_timer(0.05), "timeout")
+		tank = tanks_allowed_to_act.front() as Vehicle
+	return tank
+
+#Returns the array of tanks with higher (or lower) inititive.
+func _get_next_group_to_act(comparator: FuncRef) -> Array:
 	var ret := []
 	var ret_initiative: int
 	var one_children_found: bool = false
@@ -34,36 +72,39 @@ func _get_tanks_by_initiative(comparator: FuncRef) -> Array:
 				ret.append(child)
 	return ret
 
-func _reset_acted()->void:
-	for i in get_children():
-		(i as Vehicle).has_acted = false
-
-func _more_to_act() -> bool:
-	for i in get_children():
-		if not (i as Vehicle).has_acted:
-			return true
-	return false
-
-func _select(tanks_allowed_to_move: Array)->void:
-	for i in tanks_allowed_to_move:
+#Waits until the user clicks in one of the tanks passed in the parameter
+func _select(tanks_allowed_to_act: Array)->void:
+	for i in tanks_allowed_to_act:
 		(i as Vehicle).set_selectable(true)
-	_connect()
+		(i as Vehicle).connect("vehicle_selected", self, "_vehicle_selected")
 	var vehicle = yield(self, "vehicle_selected")
-	_disconnect()
+	for i in tanks_allowed_to_act:
+		(i as Vehicle).set_selectable(false)
+		(i as Vehicle).disconnect("vehicle_selected", self, "_vehicle_selected")
 	return vehicle
 
-func _get_next_tank(comparator: FuncRef):
-	var tanks_allowed_to_move := _get_tanks_by_initiative(comparator)
-	var tank: Vehicle
-	if tanks_allowed_to_move.size() > 1:
-		tank = yield(_select(tanks_allowed_to_move), "completed") as Vehicle
-	else:
-		#If this branch doesn't yield, the returned object is not a coroutine and the code doesn't works
-		#so we have to fake a yield.
-		yield(get_tree().create_timer(0.05), "timeout")
-		tank = tanks_allowed_to_move.front() as Vehicle
-	tank.set_selectable(false)
-	return tank
+func _vehicle_selected(vehicle):
+	emit_signal("vehicle_selected", vehicle)
+
+	
+#-----------------------------------------
+# Tanks shooting
+#-----------------------------------------
+func shoot_with_tanks() -> void:
+	_reset_acted()
+	while _more_to_act():
+		var shooting_tank:Vehicle = yield(_select_tank_by_initiative(funcref(self, "gt")), "completed")
+		shooting_tank.mark_shooting(true)
+		var target_tank:Vehicle = yield(_select_target_tank(shooting_tank), "completed")
+		shooting_tank.mark_shooting(false)
+		yield(shooting_tank.shoot_tank(target_tank), "completed")
+		shooting_tank.has_acted = true
+
+func _select_target_tank(shooting_tank:Vehicle) -> void:
+	_set_targetable_tanks(shooting_tank, true)
+	var vehicle = yield(self, "vehicle_selected")
+	_set_targetable_tanks(shooting_tank, false)
+	return vehicle	
 
 func _set_targetable_tanks(shooting_tank:Vehicle, targetable: bool) -> void:
 	for i in get_children():
@@ -76,29 +117,9 @@ func _set_targetable_tanks(shooting_tank:Vehicle, targetable: bool) -> void:
 				else:
 					v.disconnect("vehicle_selected", self, "_vehicle_selected")
 
-func _get_target_tank(shooting_tank:Vehicle) -> void:
-	_set_targetable_tanks(shooting_tank, true)
-	var vehicle = yield(self, "vehicle_selected")
-	_set_targetable_tanks(shooting_tank, false)
-	return vehicle
-	
-func move_tanks() -> void:
-	_reset_acted()
-	while _more_to_act():
-		var tank:Vehicle = yield(_get_next_tank(funcref(self, "lt")), "completed")
-		yield(tank.move_tank(), "completed")
-		tank.has_acted = true
-
-func shoot_with_tanks() -> void:
-	_reset_acted()
-	while _more_to_act():
-		var shooting_tank:Vehicle = yield(_get_next_tank(funcref(self, "gt")), "completed")
-		shooting_tank.mark_shooting(true)
-		var target_tank:Vehicle = yield(_get_target_tank(shooting_tank), "completed")
-		shooting_tank.mark_shooting(false)
-		yield(shooting_tank.shoot_tank(target_tank), "completed")
-		shooting_tank.has_acted = true
-
+#-----------------------------------------
+# Tanks command
+#-----------------------------------------
 func command_tanks() -> void:
 	for i in get_children():
 		(i as Vehicle).set_movement_token(0)
