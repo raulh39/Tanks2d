61fef85e409ee4738cbcf74b7435504a31496eda
Raul Huertas <raulh39@gmail.com>
2019-11-23 12:34:33 +0100

    Manually restore HUD size when toggling info

--- Code/GUI.gd
+++ Code/GUI.gd
@@ -4,7 +4,8 @@ class_name Gui
 
 enum Phases { MOVE, SHOOT, COMMAND }
 
-onready var _phase_label: Label = ($CenterContainer/MainContainer/Phase as Label)
+onready var _phase_label: Label = ($MainContainer/Phase as Label)
+onready var _min_y_rect_size := rect_size.y
 
 func setPhase(new_phase: int) -> void:
 	match new_phase:
@@ -14,3 +15,9 @@ func setPhase(new_phase: int) -> void:
 			_phase_label.text = "Shooting Phase"
 		Phases.COMMAND:
 			_phase_label.text = "Command Phase"
+
+
+func _input(event):
+	if event.is_action_released("toggle"):
+		$MainContainer/StatisticsContainer.visible = not $MainContainer/StatisticsContainer.visible
+		rect_size.y = _min_y_rect_size
