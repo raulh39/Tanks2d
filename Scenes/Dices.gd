a66973c4cb48a5a6dad9d1bcd186707b81f5e652
Raul Huertas <raulh39@gmail.com>
2019-12-01 11:07:18 +0100

    Refactor dices

--- Scenes/Dices.gd
+++ Scenes/Dices.gd
@@ -6,29 +6,32 @@ onready var del_button := $DelButton
 
 export (float, 0, 10) var rolling_time = 1.0
 export (float,0,10) var dice_margin = 2
-export (float) var child_dices_position_y = 200
 export (int,1,10) var max_number_of_dices = 5
 
 var _dice_scene := preload("res://Scenes/Dice.tscn")
 var _child_dices := []
-var _next_child_position_x := 100
+var _next_child_position_x :float
 var _numbers_rolled := []
 
 func _ready():
-	_on_AddButton_pressed() #We want at least one dice
+	_next_child_position_x = $FirstDice.position.x
+	_add_dice($FirstDice)
+
+func _add_dice(new_dice: Sprite) -> void:
+	_child_dices.append(new_dice)
+	new_dice.connect("rolled", self, "_on_Rolled")
+	_next_child_position_x += dice_margin + $FirstDice.get_size().x
 
 func _on_AddButton_pressed():
-	if _child_dices.size() >= max_number_of_dices:
+	if _child_dices.size() == max_number_of_dices:
 		return
 	var new_dice := _dice_scene.instance()
 	add_child(new_dice)
-	new_dice.connect("rolled", self, "_on_Rolled")
-	_child_dices.append(new_dice)
-	new_dice.position = Vector2(_next_child_position_x, child_dices_position_y)
-	_next_child_position_x += dice_margin + new_dice.get_size().x
+	new_dice.position = Vector2(_next_child_position_x, $FirstDice.position.y)
+	_add_dice(new_dice)
 	
 func _on_DelButton_pressed():
-	if _child_dices.size() <= 1:
+	if _child_dices.size() == 1:
 		return
 	var old_dice: Node = _child_dices.pop_back()
 	_next_child_position_x = old_dice.position.x
