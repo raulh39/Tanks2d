fd0935c8fe99a3033a1a127332a4439eb04bb752
Raul Huertas <raulh39@gmail.com>
2019-12-06 11:13:47 +0100

    Randomize initial dices rotation and velocities

--- Code/Dice.gd
+++ Code/Dice.gd
@@ -1,3 +1,24 @@
 extends RigidBody
 
 class_name Dice
+
+const MAX_ANGULAR_VELOCITY := 20
+const MAX_LINEAR_VELOCITY := 20
+
+func _ready():
+	self.angular_velocity = Vector3(
+									rand_range(-MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY),
+									rand_range(-MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY),
+									rand_range(-MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY)
+							)
+	self.linear_velocity = Vector3(
+									rand_range(-MAX_LINEAR_VELOCITY, MAX_LINEAR_VELOCITY),
+									rand_range(-MAX_LINEAR_VELOCITY, MAX_LINEAR_VELOCITY),
+									rand_range(-MAX_LINEAR_VELOCITY, MAX_LINEAR_VELOCITY)
+							)
+	self.rotation_degrees = Vector3(
+									rand_range(0, 360),
+									rand_range(0, 360),
+									rand_range(0, 360)
+							)
+	
