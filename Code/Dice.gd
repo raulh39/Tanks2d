4250c6f934c68dafbb895dfa10c6cc0c69b3c3f0
Raul Huertas <raulh39@gmail.com>
2019-12-06 16:17:37 +0100

    Add function to read dice results

--- Code/Dice.gd
+++ Code/Dice.gd
@@ -5,6 +5,13 @@ class_name Dice
 const MAX_ANGULAR_VELOCITY := 20
 const MAX_LINEAR_VELOCITY := 20
 
+onready var rc_1 := $rc_1
+onready var rc_2 := $rc_2
+onready var rc_3 := $rc_3
+onready var rc_4 := $rc_4
+onready var rc_5 := $rc_5
+onready var rc_6 := $rc_6
+
 func _ready():
 	self.angular_velocity = Vector3(
 									rand_range(-MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY),
@@ -21,4 +28,12 @@ func _ready():
 									rand_range(0, 360),
 									rand_range(0, 360)
 							)
-	
+
+func number_shown() -> int:
+	if rc_1.is_colliding(): return 1
+	if rc_2.is_colliding(): return 2
+	if rc_3.is_colliding(): return 3
+	if rc_4.is_colliding(): return 4
+	if rc_5.is_colliding(): return 5
+	if rc_6.is_colliding(): return 6
+	return 0
