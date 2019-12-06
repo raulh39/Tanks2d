f48d084b6d8638042966685af782db56351b4fca
Raul Huertas <raulh39@gmail.com>
2019-12-06 09:38:25 +0100

    Working mock for 3d dice rolling

--- Code/Main.gd
+++ Code/Main.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 onready var _gui := $CanvasLayer/GUI
+
 var _shooting:=false
 
 func _ready():
+	var _3d_viewport := $Viewport
+	var _viewport_sprite := $CanvasLayer/Container3D/ViewportSprite
+	_3d_viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	# Let two frames pass to make sure the screen was captured
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	_viewport_sprite.texture = _3d_viewport.get_texture()
+	
 	while true:
 		$CanvasLayer/GUI.setPhase(Gui.Phases.MOVE)
 		yield($TankList.move_tanks(), "completed")
